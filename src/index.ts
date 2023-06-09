6552c4f0392ddd43fce18f8d7e123a99b71b0c30
Jadiscke <vjtasso@gmail.com>
2023-06-09 11:04:54 -0300

    update tests and index

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,6 @@ import { createServer } from  './server';
 
 const app = createServer();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
 
 app.listen(3000, () => {
     console.log('app listening on port 3000!');
